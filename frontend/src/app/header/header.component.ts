5833b2e7bc6283ab1105d652381478488ee1f3f2
Sebastian Cataldo Smith <sebacataldos@gmail.com>
2021-06-19 21:15:22 -0400

    Cambios al login
    -Añadidos mas metodos para obtener la informacion sobre la sesion local del usuario.
    -El header ha cambiado, ahora actualiza la cabezera del sitio de manera dinamica al refrescar la pestaña.

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,5 +1,5 @@
-import { HeaderLoginService } from './../servicios/header-login.service';
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../servicios/login.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -8,13 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   public nombreUsuario: string = '';
   public usuarioIniciado: boolean = false;
-  constructor(private headerLog:HeaderLoginService) {}
+  public esAdmin: boolean = false;
+  constructor(
+    private login: LoginService,
+  ) {}
 
   ngOnInit(): void {
-    this.headerLog.headerTrigger.subscribe(data=>{
-      this.nombreUsuario=data.nombres+' '+data.apellidos;
-      this.usuarioIniciado=true;
-      console.log(this.nombreUsuario);
-    });
+    if (this.login.logueado())this.nombreUsuario=this.login.getNombreUsuario();
+    this.nombreUsuario =this.login.getNombreUsuario();
+    if (sessionStorage.getItem('esAdmin') == 'true') this.esAdmin = true;
+    if(sessionStorage.getItem('enSesion'))this.usuarioIniciado = true;
   }
 }
--- frontend/src/app/servicios/login.service.ts
+++ frontend/src/app/servicios/login.service.ts
@@ -7,8 +7,38 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class LoginService {
-  public idUsuario:number;
   constructor(private http: HttpClient) {}
+  /**
+   * Loguea al usuario segun su rol
+   * @param esAdmin si es admin o no
+   * @param id la id del usuario o admin
+   */
+  setLogueoStatus(esAdmin: string, id: number) {
+    sessionStorage.setItem('esAdmin', esAdmin);
+    sessionStorage.setItem('enSesion', 'true');
+    sessionStorage.setItem('idUsuario', id.toString());
+    this.getUser().subscribe(data=>{
+      sessionStorage.setItem('nombreUsuario', data.message.nombres+' '+data.message.apellidos);
+    });
+  }
+  getNombreUsuario(): string {
+    return sessionStorage.getItem('nombreUsuario');
+  }
+  /**
+   * 
+   * @returns devuelve la id del usuario o admin.
+   */
+  getIdUsuario(): number {
+    return Number.parseInt(sessionStorage.getItem('idUsuario'));
+  }
+  /**
+   * Verifica que el usuario se encuentra logueado
+   * @returns si el usuario está logueado o no
+   */
+  logueado(): boolean {
+    if (sessionStorage.getItem('enSesion') != null) return true;
+    else return false;
+  }
   validarLoginUser(
     usuario: string,
     password: string,
@@ -17,17 +47,34 @@ export class LoginService {
     let headers = new HttpHeaders();
     headers = headers.append('Content-Type', 'application/json');
     headers = headers.append('access-token', token);
-   
-    return this.http.get(`${environment.apiLoginUser}/login/user?email=${usuario}&password=${password}`,{ headers: headers });
+
+    return this.http.get(
+      `${environment.apiLoginUser}/login/user?email=${usuario}&password=${password}`,
+      { headers: headers }
+    );
   }
-  validarLoginAdmin(usuario: string,password: string,token: string): Observable<any> {
+  validarLoginAdmin(
+    usuario: string,
+    password: string,
+    token: string
+  ): Observable<any> {
     let headers = new HttpHeaders();
     headers = headers.append('Content-Type', 'application/json');
     headers = headers.append('access-token', token);
-    
-    return this.http.get(`${environment.apiLoginAdmin}?email=${usuario}&password=${password}`,{ headers: headers });
+
+    return this.http.get(
+      `${environment.apiLoginAdmin}?email=${usuario}&password=${password}`,
+      { headers: headers }
+    );
   }
+  /**
+   * Solicita el token.
+   * @returns devuelve el token
+   */
   token(): Observable<any> {
     return this.http.get(`${environment.apiToken}`);
+  }
+  getUser(): Observable<any> {
+    return this.http.get(`${environment.apiGetUser}${sessionStorage.getItem('idUsuario')}`);
   }
 }
