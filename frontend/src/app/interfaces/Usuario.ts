4519efa924afd914be5529d00dc5ba5f58c60d02
Sebastian Cataldo Smith <sebacataldos@gmail.com>
2021-06-19 21:15:50 -0400

    .

--- frontend/src/app/interfaces/Usuario.ts
+++ frontend/src/app/interfaces/Usuario.ts
@@ -1,9 +1,36 @@
-export interface Ticket{
-  prioridad_idPrioridad:number;
-  asunto:string;
-  descripcion:string;
-  categoria:string;
-  usuario_idUsuario:number;
+/**
+ * Corresponde al ticket que visualiza el administrador
+ * 
+ */
+export interface TicketForm {
+  prioridad_idPrioridad: number;
+  asunto: string;
+  descripcion: string;
+  categoria: string;
+  usuario_idUsuario: number;
+  idFormulario:number;
+}
+/**
+ * Corresponde al form que el usuario llena en la web,
+ * ignorando el idFormulario que solamente es obtenido
+ * desde la base de datos.
+ */
+export interface FormReclamo {
+  prioridad_idPrioridad: number;
+  asunto: string;
+  descripcion: string;
+  categoria: string;
+  usuario_idUsuario: number;
+}
+/**
+ * Ticket generado luego de que el usuario complete
+ * el formulario.
+ */
+export interface Ticket {
+  respuesta: string;
+  estado: string;
+  formulario_idFormulario: number;
+  prioridad_idPrioridad: number;
 }
 export interface Usuario {
   nombres: string;
