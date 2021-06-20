bc1cd97e2f2f4fc599853092f951a55835a4624d
Sebastian Cataldo Smith <sebacataldos@gmail.com>
2021-06-19 21:06:56 -0400

    Cambios menores

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+import { GuardGuard } from './components/guard/guard.guard';
+import { LoginService } from './servicios/login.service';
+import { RecuperarPassComponent } from './components/recuperar-pass/recuperar-pass.component';
 import { IniciarSesionAdminComponent } from './components/admin/iniciar-sesion-admin/iniciar-sesion-admin.component';
 import { IniciarSesionUsuarioComponent } from './components/sesion/sesion.component';
 import { AdminHomeComponent } from './components/admin-home/admin-home.component';
@@ -8,23 +11,61 @@ import { EnviarRespuestaComponent } from './components/admin/enviar-respuesta/en
 import { RegistroComponent } from './components/registro/registro.component';
 import { UsuarioIniciadoComponent } from './components/usuario-iniciado/usuario-iniciado.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component:IniciarSesionUsuarioComponent },
-  { path: 'adminIniciar',component:IniciarSesionAdminComponent},
-  { path: 'usuarioHome', component: UsuarioIniciadoComponent },
+  { path: '', component: IniciarSesionUsuarioComponent },
+  { path: 'adminIniciar', component: IniciarSesionAdminComponent },
+  {
+    path: 'usuarioHome',
+    component: UsuarioIniciadoComponent,
+    canActivate: [GuardGuard],
+    data: {
+      rol: 'user',
+    },
+  },
   { path: 'registro', component: RegistroComponent },
   {
     path: 'adminHome',
     component: AdminHomeComponent,
+    data: {
+      rol: 'admin',
+    },
+    canActivate: [GuardGuard],
     children: [
-      { path: 'reporteUsuarios', component: ReporteUsuariosComponent },
-      { path: 'reporteSolicitudes', component: ReporteSolicitudesComponent },
+      {
+        path: 'reporteUsuarios',
+        component: ReporteUsuariosComponent,
+        data: {
+          rol: 'admin',
+        },
+      },
+      {
+        path: 'reporteSolicitudes',
+        data: {
+          rol: 'admin',
+        },
+        component: ReporteSolicitudesComponent,
+      },
     ],
   },
-  { path: 'enviarRespuesta', component: EnviarRespuestaComponent },
-  { path: 'cambiarEstado', component: CambiarEstadoComponent },
+  {
+    path: 'enviarRespuesta',
+    canActivate: [GuardGuard],
+    data: {
+      rol: 'admin',
+    },
+    component: EnviarRespuestaComponent,
+  },
+  {
+    path: 'cambiarEstado',
+    canActivate: [GuardGuard],
+    data: {
+      rol: 'admin',
+    },
+    component: CambiarEstadoComponent,
+  },
+  { path: 'recuperacion', component: RecuperarPassComponent },
 ];
 
 @NgModule({
