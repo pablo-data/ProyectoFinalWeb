ec805ca45935d2a9d9464bc0992e7c70e13cc433
Sebastian Cataldo Smith <sebacataldos@gmail.com>
2021-06-27 20:33:58 -0400

    Se han corregido ciertos estilos.
    -Se ha corregido el recaptcha.
    -Se ha centrado el <a> de registrarse en inicio de sesion del usuario.

--- frontend/src/app/components/user/sesion/sesion.component.ts
+++ frontend/src/app/components/user/sesion/sesion.component.ts
@@ -2,7 +2,7 @@ import { LoginService } from '../../../servicios/login.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { HostListener } from '@angular/core';
 @Component({
   selector: 'app-sesion',
   templateUrl: './sesion.component.html',
@@ -10,15 +10,28 @@ import { Router } from '@angular/router';
 })
 export class IniciarSesionUsuarioComponent implements OnInit {
   @Input() data: any;
+  screenHeight: any;
+  screenWidth: any;
   public formLogin: FormGroup = new FormGroup({});
   public siteKey: string = '';
   public llenadoCompleto: boolean = false;
-  public mensaje:string='';
+  public mensaje: string = '';
+  public theme: 'light' | 'dark' = 'dark';
+  public size: 'compact' | 'normal' = 'normal';
+  @HostListener('window:resize', ['$event'])
+  onResize(event?) {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+    if (this.screenWidth <= 348) this.size = 'compact';
+    else this.size = 'normal';
+  }
+
   constructor(
     private formBuilder: FormBuilder,
     public router: Router,
-    private login: LoginService,
+    private login: LoginService
   ) {
+    this.onResize();
     this.siteKey = '6LdZ8CgbAAAAACt5zy_JxiKcrrDJKjSeKfw-9Wf6';
   }
 
@@ -30,20 +43,22 @@ export class IniciarSesionUsuarioComponent implements OnInit {
       recaptcha: ['', Validators.required],
     });
   }
-  recuperarPass(){
-    sessionStorage.setItem('recuperarAdmin','false');
+  recuperarPass() {
+    sessionStorage.setItem('recuperarAdmin', 'false');
     this.router.navigate(['/recuperacion']);
   }
   send() {
-    this.login.token().subscribe(token => {
-      this.login.validarLoginUser(
+    this.login.token().subscribe((token) => {
+      this.login
+        .validarLoginUser(
           this.formLogin.get('email').value,
           this.formLogin.get('password').value,
           token.message
-        ).subscribe((data) => {
-          if (data.message=='') {
+        )
+        .subscribe((data) => {
+          if (data.message == '') {
             console.log('Login no existe');
-            this.mensaje="Usuario no encontrado";
+            this.mensaje = 'Usuario no encontrado';
           } else {
             this.data = data;
             this.mensaje = '';
